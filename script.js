486be7b6844e744b4dd1159583d0d9a3f26e310f
Andrew Martin <martinandrew2@johndeere.com>
2020-09-11 17:36:14 -0500

    Added end screen elements and logic. Working on refactoring some of the code

--- script.js
+++ script.js
@@ -1,11 +1,72 @@
-function loseSequence() {
-    this.classList.add('mine');
+function difficultyScreen() {
+    const diffButtons = gameOverlay.querySelector('#difficultyButtons');
+    const diff = gameOverlay.querySelector('#difficulty');
+    const play = gameOverlay.querySelector('#play');
+    const easy = gameOverlay.querySelector('#easy');
+    const medium = gameOverlay.querySelector('#medium');
+    const hard = gameOverlay.querySelector('#hard');
 
-    gameOverlay.style.display = 'block';
+    diffButtons.style.display = 'flex';
+    diff.style.display = 'block';
+    play.style.display = 'block';
+
+    function newBoard(row,col,mine) {
+        resetBoard(row,col,mine);
+        play.addEventListener('click',() => {
+            gameOverlay.style.display = 'none'
+            diffButtons.style.display = 'none';
+            diff.style.display = 'none';
+            play.style.display = 'none';
+        });
+    }
+
+    easy.addEventListener('click',() => {
+        newBoard(10,10,15);
+    })
+
+    medium.addEventListener('click',() => {
+        newBoard(20,20,60);
+    })
+    
+    hard.addEventListener('click',() => {
+        newBoard(20,40,120);
+    })
+
+
+}
+
+function loseSequence() {
+    addMine.bind(this)();
+
+    gameOverlay.style.display = 'flex';
+
+    const report = gameOverlay.querySelector('#gameReport');
+    const text = gameOverlay.querySelector('#headerText')
+    const yes = gameOverlay.querySelector('#yes');
+    const no = gameOverlay.querySelector('#no');
+    const cont = gameOverlay.querySelector('#continue');
+    const playButtons = gameOverlay.querySelector('#playButtons');
+
+    report.style.display = 'block';
+    playButtons.style.display = 'flex';
+    cont.style.display = 'none';
+
+    yes.addEventListener('click',()=> {
+        playButtons.style.display = 'none';
+        report.style.display = 'none';
+        difficultyScreen();
+    });
+
+    no.addEventListener('click',() => {
+        text.innerText = 'Cmon now. Play again. Do it.'
+        no.style.display = 'none';
+    })
 }
 
 function winSequence() {
-    gameOverlay.style.display = 'block';
+    gameOverlay.style.display = 'flex';
+
+
 }
 
 function clearZeros() {
@@ -18,14 +79,14 @@ function clearZeros() {
             const col = startCol + b;
             
             const notBelowGrid = row >= 0 && col >= 0;
-            const notAboveGrid = row < rows && col < cols;
+            const notAboveGrid = row < gridRows && col < gridCols;
             const notCurrentGrid = !((a === 0) && (b === 0));
 
             if (notBelowGrid && notAboveGrid && notCurrentGrid)  {
                 const gridCol = document.querySelectorAll(`[data-col='${col}']`);
                 const gridSqr = [...gridCol].filter((sqr) => sqr.dataset.row === `${row}`);
 
-                if (gridSqr[0].classList.contains('hidden')) {
+                if (gridSqr[0].parentElement.classList.contains('hidden')) {
                     revealNumber.bind(gridSqr[0])();
                 }   
             }
@@ -33,20 +94,25 @@ function clearZeros() {
     }
 }
 
+function addMine() {
+    this.classList.add('mine');
+
+}
+
 function revealNumber() {
-    this.classList.remove('hidden');
-    this.classList.add('revealed')
+    this.style.display = 'none';
+    this.parentElement.classList.remove('hidden');
 
     const gridNum = gridData[this.dataset.row][this.dataset.col];
 
     if (gridNum === 9) {
-        loseSequence.bind(this)();
+        loseSequence.bind(this.parentElement)();
     } else if (gridNum === 0) {
+        goodSquares--;
         clearZeros.bind(this)();
     } else {
-        const pTag = document.createElement('p');
+        const pTag = this.parentElement.querySelector('p');
         pTag.innerText = `${gridNum}`;
-        this.appendChild(pTag);
         goodSquares--;
     }
 
@@ -56,12 +122,12 @@ function revealNumber() {
 }
 
 function getRandomGrid() {
-    const randRow = Math.round(Math.random() * (rows-1));
-    const randCol = Math.round(Math.random() * (cols-1));
+    const randRow = Math.round(Math.random() * (gridRows-1));
+    const randCol = Math.round(Math.random() * (gridCols-1));
     return [randRow,randCol];
 }
 
-function populateGrid() {
+function populateGrid(totalMines) {
     const thisRow = parseInt(this.dataset.row);
     const thisCol = parseInt(this.dataset.col);
     
@@ -69,22 +135,22 @@ function populateGrid() {
 
     while(minesPlaced < totalMines) {
         [mineRow,mineCol] = getRandomGrid();
-        if (Math.abs(thisRow - mineRow) >= 2 && Math.abs(thisCol - mineCol) >= 2) {
+        if (Math.abs(thisRow - mineRow) >= 2 && Math.abs(thisCol - mineCol) >= 2 && gridData[mineRow][mineCol] !== 9) {
             gridData[mineRow][mineCol] = 9;
             minesPlaced++;
         }
        
     }
     
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < gridRows; i++) {
+        for (let j = 0; j < gridCols; j++) {
 
             let sum = 0;
 
             for (let a = -1; a <= 1; a++) {
                 for (let b = -1; b <= 1; b++) {
                     const notBelowGrid = (i+a) >= 0 && (j+b) >= 0;
-                    const notAboveGrid = (i+a) < rows && (j+b) < cols;
+                    const notAboveGrid = (i+a) < gridRows && (j+b) < gridCols;
                     const notCurrentGrid = !((a === 0) && (b === 0));
 
                     if (notBelowGrid && notAboveGrid && notCurrentGrid)  {
@@ -102,78 +168,95 @@ function populateGrid() {
     }
 }
 
-const gameBoard = document.querySelector('#gameBoard');
-const gameOverlay = document.querySelector('#gameOverlay');
+function resetBoard(rows,cols,totalMines) {
+    let noClicks = true;
+    goodSquares = rows*cols - totalMines;
+    
+    gridData = [];
+    gridRows = rows;
+    gridCols = cols;
 
-const rows = 20;
-const cols = 20;
-gameBoard.style['grid-template-rows'] = `repeat(${rows},20px`;
-gameBoard.style['grid-template-columns'] = `repeat(${cols},20px`;
+    gameBoard.textContent = '';
+    gameBoard.style['grid-template-rows'] = `repeat(${rows},20px`;
+    gameBoard.style['grid-template-columns'] = `repeat(${cols},20px`;
 
-let noClicks = true;
-
-const totalMines = 50;
-let goodSquares = rows*cols - totalMines;
-
-let gridData = [];
-
-for (let i = 0; i < rows; i++) {
-    let temp = [];
-    for (let j = 0; j < cols; j++) {
-        temp.push(0);
+    for (let i = 0; i < rows; i++) {
+        let temp = [];
+        for (let j = 0; j < cols; j++) {
+            temp.push(0);
+        }
+        gridData.push(temp);
     }
-    gridData.push(temp);
-}
 
-for (let i = 1; i <= rows; i++) {
-    for (let j = 1; j <= cols; j++) {
-        const gridDiv = document.createElement('div');
-        gameBoard.appendChild(gridDiv);
-        gridDiv.classList.add('mineGrid','hidden');
-        gridDiv.style['grid-column'] = `${j}`;
-        gridDiv.style['grid-row'] = `${i}`;
+    for (let i = 1; i <= rows; i++) {
+        for (let j = 1; j <= cols; j++) {
+            const gridCoveredDiv = document.createElement('div');
+            gridCoveredDiv.classList.add('gridCover');
+            gridCoveredDiv.setAttribute('data-col', j-1);
+            gridCoveredDiv.setAttribute('data-row', i-1);
 
-        gridDiv.setAttribute('data-col', j-1);
-        gridDiv.setAttribute('data-row', i-1);
-        
-        gridDiv.addEventListener('click',function clickFunction(event) {
-            const existingFlag = gridDiv.querySelector('.flag');
+            const gridRevealedDiv = document.createElement('div');
+            gridRevealedDiv.classList.add('hidden');
+            gridRevealedDiv.style['grid-column'] = `${j}`;
+            gridRevealedDiv.style['grid-row'] = `${i}`;
+            gridRevealedDiv.classList.add('mineGrid');
+            const pTag = document.createElement('p');
+            gridRevealedDiv.appendChild(pTag);
 
-            if (!existingFlag) {
-                if (noClicks) {
-                    populateGrid.bind(gridDiv)();
-                    noClicks = false;
+            gameBoard.appendChild(gridRevealedDiv);
+            gridRevealedDiv.appendChild(gridCoveredDiv);
+            
+            gridCoveredDiv.addEventListener('click',function clickFunction(event) {
+                const existingFlag = gridCoveredDiv.querySelector('.flag');
+
+                if (!existingFlag) {
+                    if (noClicks) {
+                        populateGrid.bind(gridCoveredDiv)(totalMines);
+                        noClicks = false;
+                    }
+
+                    gridCoveredDiv.style['box-shadow'] = '0 0 3px 0 black inset';
+
+                    setTimeout(() => {
+                        revealNumber.bind(gridCoveredDiv)();
+
+                        gridCoveredDiv.style['box-shadow'] = '';
+                        
+                    },80)
+
+                    gridCoveredDiv.removeEventListener('click',clickFunction);
+                }
+            })
+
+            gridCoveredDiv.addEventListener('contextmenu', function addFlag(e) {
+                e.preventDefault();
+
+                const existingFlag = gridCoveredDiv.querySelector('img');
+
+                if (!existingFlag) {
+                    const flagImg = document.createElement('img');
+                    flagImg.setAttribute('src','images/flag.png');
+                    flagImg.classList.add('flag');
+                    gridCoveredDiv.appendChild(flagImg);
+                } else {
+                    existingFlag.remove();
                 }
 
-                gridDiv.style['box-shadow'] = '0 0 3px 0 black inset';
-
-                setTimeout(() => {
-                    revealNumber.bind(gridDiv)();
-
-                    gridDiv.style['box-shadow'] = '';
-                    
-                },80)
-
-                gridDiv.removeEventListener('click',clickFunction);
-            }
-        })
-
-        gridDiv.addEventListener('contextmenu', (e) => {
-            e.preventDefault();
-
-            const existingFlag = gridDiv.querySelector('img');
-
-            if (!existingFlag) {
-                const flagImg = document.createElement('img');
-                flagImg.setAttribute('src','images/flag.png');
-                flagImg.classList.add('flag');
-                gridDiv.appendChild(flagImg);
-            } else {
-                existingFlag.remove();
-            }
-
-            return false;
-        })
+                return false;
+            })
+        }
     }
 }
 
+const gameBoard = document.querySelector('#gameBoard');
+const gameOverlay = document.querySelector('#gameOverlay');
+let goodSquares;
+let gridRows;
+let gridCols;
+let gridData;
+
+// const rows = 10;
+// const cols = 10;
+// const totalMines = 20;
+
+resetBoard(10,10,15);
